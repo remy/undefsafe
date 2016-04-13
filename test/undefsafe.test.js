fff7997dbf6b29e1411ee51069d436f115f43b43
Remy Sharp <remy@remysharp.com>
2016-04-13 21:54:30 +0100

    refactor: move to tap over mocha

--- test/undefsafe.test.js
+++ test/undefsafe.test.js
@@ -1,84 +1,88 @@
 'use strict';
-/*global describe:true, it: true */
-var assert = require('assert');
+var test = require('tap').test;
 var undefsafe = require('../lib/undefsafe');
 
-describe('undefsafe', function () {
-  it('should handle primatives', function () {
-    var r = undefsafe(1, '');
-    assert(r === 1, 'undefsafe is 1: ' + r);
-  });
+test('should handle primatives', function (t) {
+  var r = undefsafe(1, '');
+  t.equal(r, 1, 'undefsafe is 1: ' + r);
+  t.end();
+});
 
-  it('should handle empty objects', function () {
-    var value = {};
-    var r;
+test('should handle empty objects', function (t) {
+  var value = {};
+  var r;
 
-    r = undefsafe(value, '');
-    assert(r === value, 'value is value' + r);
+  r = undefsafe(value, '');
+  t.equal(r, value, 'value is value' + r);
 
-    r = undefsafe(value, 'foo');
-    assert(r === undefined, 'value.foo is undefined: ' + r);
+  r = undefsafe(value, 'foo');
+  t.equal(r, undefined, 'value.foo is undefined: ' + r);
 
-    r = undefsafe(value, 'foo.bar');
-    assert(r === undefined, 'value.foo.bar is undefined: ' + r);
-  });
+  r = undefsafe(value, 'foo.bar');
+  t.equal(r, undefined, 'value.foo.bar is undefined: ' + r);
 
-  it('should handle null properties', function () {
-    var value = {
-      a: {
-        b: null,
-      },
-    };
-    var r;
+  t.end();
+});
 
-    r = undefsafe(value, 'a.b');
-    assert(r === null, 'value.a.b is null: ' + r);
+test('should handle null properties', function (t) {
+  var value = {
+    a: {
+      b: null,
+    },
+  };
+  var r;
 
-    r = undefsafe(value, 'a.b.c');
-    assert(r === undefined, 'value.a.b.c is undefined: ' + r);
-  });
+  r = undefsafe(value, 'a.b');
+  t.equal(r, null, 'value.a.b is null: ' + r);
 
-  it('should find deep object properties', function () {
-    var value = {
-      a: {
-        b: {
-          c: {
-            d: 10,
-            e: {
-              f: 20,
-            },
-            g: true,
-            h: false,
-            i: undefined,
-            j: null,
+  r = undefsafe(value, 'a.b.c');
+  t.equal(r, undefined, 'value.a.b.c is undefined: ' + r);
+
+  t.end();
+});
+
+test('should find deep object properties', function (t) {
+  var value = {
+    a: {
+      b: {
+        c: {
+          d: 10,
+          e: {
+            f: 20,
           },
+          g: true,
+          h: false,
+          i: undefined,
+          j: null,
         },
       },
-    };
-    var r;
+    },
+  };
+  var r;
 
-    r = undefsafe(value, 'a');
-    assert(r === value.a, 'value.a: ' + r + ' ' + value.a);
+  r = undefsafe(value, 'a');
+  t.equal(r, value.a, 'value.a: ' + r + ' ' + value.a);
 
-    r = undefsafe(value, 'a.foo');
-    assert(r === undefined, 'value.a.foo is undefined: ' + r);
+  r = undefsafe(value, 'a.foo');
+  t.equal(r, undefined, 'value.a.foo is undefined: ' + r);
 
-    r = undefsafe(value, 'a.b.c.d');
-    assert(r === 10, 'value.a.b.c.d = 10');
+  r = undefsafe(value, 'a.b.c.d');
+  t.equal(r, 10, 'value.a.b.c.d = 10');
 
-    r = undefsafe(value, 'a.b.c.e');
-    assert(r === value.a.b.c.e, 'value.a.b.c.e = <object>');
+  r = undefsafe(value, 'a.b.c.e');
+  t.equal(r, value.a.b.c.e, 'value.a.b.c.e = <object>');
 
-    r = undefsafe(value, 'a.b.c.g');
-    assert(r === true, 'bool(true)');
+  r = undefsafe(value, 'a.b.c.g');
+  t.equal(r, true, 'bool(true)');
 
-    r = undefsafe(value, 'a.b.c.h');
-    assert(r === false, 'bool(false)');
+  r = undefsafe(value, 'a.b.c.h');
+  t.equal(r, false, 'bool(false)');
 
-    r = undefsafe(value, 'a.b.c.i');
-    assert(r === undefined, 'undefined');
+  r = undefsafe(value, 'a.b.c.i');
+  t.equal(r, undefined, 'undefined');
 
-    r = undefsafe(value, 'a.b.c.j');
-    assert(r === null, 'null');
-  });
+  r = undefsafe(value, 'a.b.c.j');
+  t.equal(r, null, 'null');
+
+  t.end();
 });
