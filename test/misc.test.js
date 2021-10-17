58fc47439a3bb2cd1cccdaeb8777561a325fba1c
Remy Sharp <remy@remysharp.com>
2021-10-17 10:10:34 +0100

    fix: remove debug and add tests
    Fixes #12

--- test/misc.test.js
+++ test/misc.test.js
@@ -1,11 +1,20 @@
 var test = require('tap').test;
 var undefsafe = require('../lib/undefsafe');
 
-test('cannot modify prototype chain', function(t) {
+test('cannot modify prototype chain', function (t) {
   const pre = {}.__proto__.toString;
   var payload = '__proto__.toString';
   undefsafe({ a: 'b' }, payload, 'JHU');
   t.notEqual({}.toString, 'JHU');
   ({}.__proto__.toString = pre); // restore
+  t.end();
+});
+
+test('cannot pollute prototype chain', function (t) {
+  global.polluted = 'nope';
+  undefsafe({}, '__proto__.polluted', true);
+  t.notEqual(global.polluted, true);
+  t.equal(global.polluted, 'nope');
+
   t.end();
 });
